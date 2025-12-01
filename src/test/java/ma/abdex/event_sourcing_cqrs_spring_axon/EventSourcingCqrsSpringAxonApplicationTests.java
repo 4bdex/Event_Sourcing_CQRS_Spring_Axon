3d5cd5ef72a3eba39596cf9c61d1da307b1bfa26
Abdex <abdex4social@gmail.com>
2025-12-01 12:56:31 +0100

    init

---
+++ src/test/java/ma/abdex/event_sourcing_cqrs_spring_axon/EventSourcingCqrsSpringAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.abdex.event_sourcing_cqrs_spring_axon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingCqrsSpringAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
